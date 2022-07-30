6264736275511ccd054e2067dae6b835f174c526
TakLee96 <jiahang_li@outlook.com>
2022-07-30 12:09:56 -0700

    converge reward model

--- scripts/train_ppo_pretrained_image_v0.sh
+++ scripts/train_ppo_pretrained_image_v0.sh
@@ -2,4 +2,4 @@ PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v0' \
     --model='ImageActorCritic' \
     --exp_name='ppo_pretrained_image_v0' \
     --freeze --cpu=2 \
-    --savedir="./logs/reward_model/step-199.pth"
+    --savedir="./logs/reward_model/step-999.pth"
---
+++ scripts/vis_ppo_pretrained_image_v0.sh
@@ -0,0 +1,1 @@
+PYTHONPATH=$(pwd) python spinup/plot.py ./logs/ppo_pretrained_image_v0/ppo_pretrained_image_v0_s0
