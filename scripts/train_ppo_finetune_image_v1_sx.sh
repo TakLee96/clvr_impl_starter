5fe58833f0a34eed52747238af550746099e6d5a
TakLee96 <jiahang_li@outlook.com>
2022-08-02 19:47:05 -0700

    add logs

---
+++ scripts/train_ppo_finetune_image_v1_sx.sh
@@ -0,0 +1,17 @@
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
+    --model='ImageActorCritic' \
+    --exp_name='ppo_finetune_image_v1' \
+    --cpu=1 --steps=1000 --epochs 200 --seed=0 \
+    --savedir="./logs/reward_model/step-999.pth"
+
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
+    --model='ImageActorCritic' \
+    --exp_name='ppo_finetune_image_v1' \
+    --cpu=1 --steps=1000 --epochs 200 --seed=1 \
+    --savedir="./logs/reward_model/step-999.pth"
+
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
+    --model='ImageActorCritic' \
+    --exp_name='ppo_finetune_image_v1' \
+    --cpu=1 --steps=1000 --epochs 200 --seed=2 \
+    --savedir="./logs/reward_model/step-999.pth"
