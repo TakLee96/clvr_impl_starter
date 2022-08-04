9b74a389253cedccf17212e22ccbbe33843d4e14
TakLee96 <jiahang_li@outlook.com>
2022-08-04 09:41:59 -0700

    add training evidence for gamma != 0

--- scripts/train_ppo_pretrained_image_v1_sx.sh
+++ scripts/train_ppo_pretrained_image_v1_sx.sh
@@ -1,6 +1,12 @@
 PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
     --model='ImageActorCritic' \
     --exp_name='ppo_pretrained_image_v1' \
+    --freeze --cpu=1 --steps=1000 --epochs 200 --seed=0 \
+    --savedir="./logs/reward_model/step-999.pth"
+
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
+    --model='ImageActorCritic' \
+    --exp_name='ppo_pretrained_image_v1' \
     --freeze --cpu=1 --steps=1000 --epochs 200 --seed=1 \
     --savedir="./logs/reward_model/step-999.pth"
 
