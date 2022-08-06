b1c514d8ecef62ba472f137d4e5dbc8d9153e092
TakLee96 <jiahang_li@outlook.com>
2022-08-06 10:20:57 -0700

    record overnight x 3 training progress

--- scripts/train_overnight.sh
+++ scripts/train_overnight.sh
@@ -1,68 +1,68 @@
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='SpritesState-v1' \
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='SpritesState-v2' \
     --model='MLPActorCritic' \
-    --exp_name='ppo_state_gamma0_v1' \
+    --exp_name='ppo_state_gamma0_v2' \
     --cpu=1 --steps=4000 --epochs 100 --seed=0 --gamma=0 
 
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='SpritesState-v1' \
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='SpritesState-v2' \
     --model='MLPActorCritic' \
-    --exp_name='ppo_state_gamma0_v1' \
+    --exp_name='ppo_state_gamma0_v2' \
     --cpu=1 --steps=4000 --epochs 100 --seed=1 --gamma=0 
 
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='SpritesState-v1' \
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='SpritesState-v2' \
     --model='MLPActorCritic' \
-    --exp_name='ppo_state_gamma0_v1' \
+    --exp_name='ppo_state_gamma0_v2' \
     --cpu=1 --steps=4000 --epochs 100 --seed=2 --gamma=0 
 
 
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v2' \
     --model='ImageActorCritic' \
-    --exp_name='ppo_pretrained_image_gamma0_v1' \
+    --exp_name='ppo_pretrained_image_gamma0_v2' \
     --freeze --cpu=1 --steps=4000 --epochs 500 --seed=0 --gamma=0 \
     --savedir="./logs/reward_model/step-999.pth"
 
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v2' \
     --model='ImageActorCritic' \
-    --exp_name='ppo_pretrained_image_gamma0_v1' \
+    --exp_name='ppo_pretrained_image_gamma0_v2' \
     --freeze --cpu=1 --steps=4000 --epochs 500 --seed=1 --gamma=0 \
     --savedir="./logs/reward_model/step-999.pth"
 
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v2' \
     --model='ImageActorCritic' \
-    --exp_name='ppo_pretrained_image_gamma0_v1' \
+    --exp_name='ppo_pretrained_image_gamma0_v2' \
     --freeze --cpu=1 --steps=4000 --epochs 500 --seed=2 --gamma=0 \
     --savedir="./logs/reward_model/step-999.pth"
 
 
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v2' \
     --model='ImageActorCritic' \
-    --exp_name='ppo_finetune_image_gamma0_v1' \
+    --exp_name='ppo_finetune_image_gamma0_v2' \
     --cpu=1 --steps=4000 --epochs 500 --seed=0 --gamma=0 \
     --savedir="./logs/reward_model/step-999.pth"
 
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v2' \
     --model='ImageActorCritic' \
-    --exp_name='ppo_finetune_image_gamma0_v1' \
+    --exp_name='ppo_finetune_image_gamma0_v2' \
     --cpu=1 --steps=4000 --epochs 500 --seed=1 --gamma=0 \
     --savedir="./logs/reward_model/step-999.pth"
 
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v2' \
     --model='ImageActorCritic' \
-    --exp_name='ppo_finetune_image_gamma0_v1' \
+    --exp_name='ppo_finetune_image_gamma0_v2' \
     --cpu=1 --steps=4000 --epochs 500 --seed=2 --gamma=0 \
     --savedir="./logs/reward_model/step-999.pth"
 
 
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v2' \
     --model='ImageActorCritic' \
-    --exp_name='ppo_scratch_image_gamma0_v1' \
+    --exp_name='ppo_scratch_image_gamma0_v2' \
     --cpu=1 --steps=4000 --epochs 500 --seed=0 --gamma=0
 
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v2' \
     --model='ImageActorCritic' \
-    --exp_name='ppo_scratch_image_gamma0_v1' \
+    --exp_name='ppo_scratch_image_gamma0_v2' \
     --cpu=1 --steps=4000 --epochs 500 --seed=1 --gamma=0
 
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
+PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v2' \
     --model='ImageActorCritic' \
-    --exp_name='ppo_scratch_image_gamma0_v1' \
+    --exp_name='ppo_scratch_image_gamma0_v2' \
     --cpu=1 --steps=4000 --epochs 500 --seed=2 --gamma=0
