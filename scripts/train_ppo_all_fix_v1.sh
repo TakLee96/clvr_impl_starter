d75516801ce81659c7cfb5aac99ed745dc6195e3
TakLee96 <jiahang_li@outlook.com>
2022-08-07 20:14:27 -0700

    pause at finetune_fix_s2

--- scripts/train_ppo_all_fix_v1.sh
+++ scripts/train_ppo_all_fix_v1.sh
@@ -33,17 +33,17 @@
 #     --savedir="./logs/reward_model/step-999.pth"
 
 
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
-    --model='ImageActorCritic' \
-    --exp_name='ppo_finetune_image_fix_v1' \
-    --cpu=1 --steps=4000 --epochs 750 --seed=0 \
-    --savedir="./logs/reward_model/step-999.pth"
+# PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
+#     --model='ImageActorCritic' \
+#     --exp_name='ppo_finetune_image_fix_v1' \
+#     --cpu=1 --steps=4000 --epochs 750 --seed=0 \
+#     --savedir="./logs/reward_model/step-999.pth"
 
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
-    --model='ImageActorCritic' \
-    --exp_name='ppo_finetune_image_fix_v1' \
-    --cpu=1 --steps=4000 --epochs 750 --seed=1 \
-    --savedir="./logs/reward_model/step-999.pth"
+# PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
+#     --model='ImageActorCritic' \
+#     --exp_name='ppo_finetune_image_fix_v1' \
+#     --cpu=1 --steps=4000 --epochs 750 --seed=1 \
+#     --savedir="./logs/reward_model/step-999.pth"
 
 PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v1' \
     --model='ImageActorCritic' \
--- scripts/train_ppo_all_fix_v2.sh
+++ scripts/train_ppo_all_fix_v2.sh
@@ -33,17 +33,17 @@
 #     --savedir="./logs/reward_model/step-999.pth"
 
 
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v2' \
-    --model='ImageActorCritic' \
-    --exp_name='ppo_finetune_image_fix_v2' \
-    --cpu=1 --steps=4000 --epochs 750 --seed=0 \
-    --savedir="./logs/reward_model/step-999.pth"
+# PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v2' \
+#     --model='ImageActorCritic' \
+#     --exp_name='ppo_finetune_image_fix_v2' \
+#     --cpu=1 --steps=4000 --epochs 750 --seed=0 \
+#     --savedir="./logs/reward_model/step-999.pth"
 
-PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v2' \
-    --model='ImageActorCritic' \
-    --exp_name='ppo_finetune_image_fix_v2' \
-    --cpu=1 --steps=4000 --epochs 750 --seed=1 \
-    --savedir="./logs/reward_model/step-999.pth"
+# PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v2' \
+#     --model='ImageActorCritic' \
+#     --exp_name='ppo_finetune_image_fix_v2' \
+#     --cpu=1 --steps=4000 --epochs 750 --seed=1 \
+#     --savedir="./logs/reward_model/step-999.pth"
 
 PYTHONPATH=$(pwd) python spinup/ppo.py --env='Sprites-v2' \
     --model='ImageActorCritic' \
