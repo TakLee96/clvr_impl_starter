6c778e931151fda95e1d58bceb1b3bf32e161b04
TakLee96 <jiahang_li@outlook.com>
2022-08-07 23:55:58 -0700

    add pretrained_simple and vis_lrs script

---
+++ scripts/vis_ppo_finetune_image_lrs_v0.sh
@@ -0,0 +1,5 @@
+PYTHONPATH=$(pwd) python spinup/plot.py \
+    ./logs/ppo_finetune_image_fix_v0/ \
+    ./logs/ppo_finetune_image_lr0_v0/ \
+    ./logs/ppo_finetune_image_lr1_v0/ \
+    ./logs/ppo_finetune_image_lr2_v0/ \
